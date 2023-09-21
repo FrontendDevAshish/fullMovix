2c7b7b91479d89a6be81dd1d41c9e4e8c0bbaddf
FrontendDevAshish <kumar.29ashish.95@gmail.com>
2023-09-21 10:28:45 +0530

    redux setup

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@
 import { useEffect } from 'react';
 import './App.css';
 import { fetchDataFromApi } from './Utils/api';
-
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration } from './store/homeSlice';
 
 function App() {
-  
-  
+  const dispatch = useDispatch()
+  const {url} = useSelector((state) => state.home)
+  console.log(url,'shshsghsh')
 
   const apiTest =()=>{
-    const{data} = fetchDataFromApi('/movie/popular').then((res)=>{console.log(res)})
+    const{data} = fetchDataFromApi('/movie/popular').then((res)=>{console.log(res)
+    dispatch(getApiConfiguration(res))
+    })
   }
   useEffect(()=>{apiTest()},[])
   return (
     <div className="App">
-     
+     App
+     {url?.total_pages}
     </div>
   );
 }
